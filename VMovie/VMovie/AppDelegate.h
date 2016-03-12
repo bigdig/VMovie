afa714562a3865a69a2761313fd46d242649cfbc
wyz <wyz@wyzdeMacBook-Pro.local>
2016-03-12 21:57:41 +0800

    创建项目

---
+++ VMovie/VMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VMovie
+//
+//  Created by wyz on 16/3/12.
+//  Copyright © 2016年 wyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VMovie/VMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VMovie
+//
+//  Created by wyz on 16/3/12.
+//  Copyright © 2016年 wyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
